f00b20285af5d2e5e23ca998deac50e9c01e2bcc
Abdul Matin <matinict@gmail.com>
2025-01-22 12:08:32 +0600

    Create bau.sh

---
+++ bau.sh
@@ -0,0 +1,16 @@
+    #!/bin/bash
+
+    # Update package database first
+    sudo pacman -Syy
+
+    # Get the full package list
+    pkg_list=$(pacman -Qq)
+    total_pkgs=$(echo "$pkg_list" | wc -l)
+
+    # Loop through packages in batches of 100
+    for i in $(seq 0 100 $total_pkgs); do
+        batch=$(echo "$pkg_list" | sed -n "$((i+1)),$((i+100))p")
+        if [[ -n "$batch" ]]; then
+            sudo pacman -Syu --overwrite='*' --noconfirm --needed $batch
+        fi
+    done
